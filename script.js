b823d295c5042b8db181b91500596a717b7fdb6b
Udipthishankar <Shankarudipthi@gmail.com>
2026-07-18 04:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ if (themeBtn) {
     }
   });
 }
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    const target = tab.dataset.tab;
+    tabs.forEach(t => t.classList.remove("active"));
+    contents.forEach(c => c.classList.remove("active"));
+    tab.classList.add("active");
+    document.getElementById(target).classList.add("active");
+  });
+});
